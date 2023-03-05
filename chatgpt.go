cd58b2ef40fd68d63780e1afdb68ef78534494e5
JP Etcheber <jetcheber@gmail.com>
2023-03-05 12:35:30 -0800

    get token from environment

--- chatgpt.go
+++ chatgpt.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/numbertheory/chatgpt-go/chat"
 )
 
 const usage = `Usage of ChatGPT:
 `
+const tokenNotSet = `Chat GPT token required.
+Visit https://platform.openai.com/account/api-keys to get keys for your account.
+Then, make that key an environmental variable in your shell.
+Example:
+    export CHATGPT_TOKEN=<your-token>
+`
 
 func main() {
+	token := os.Getenv("CHATGPT_TOKEN")
+	if token == "" {
+		fmt.Printf(tokenNotSet)
+		os.Exit(1)
+	}
 	for {
-		userInput := chat.StringPrompt(">")
+		userInput := chat.StringPrompt("Human>")
 		if userInput == "exit" {
 			break
 		}
-		fmt.Printf(userInput + "\n")
+		fmt.Printf(chat.SendChat(userInput) + "\n")
 	}
 
 }
