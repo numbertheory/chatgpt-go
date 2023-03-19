58097e1e5eb13bad4cdc200794db56b1fd776811
JP Etcheber <jetcheber@gmail.com>
2023-03-19 02:43:43 -0700

    trying to get record collection to self-archive

--- chatgpt.go
+++ chatgpt.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
 
 	"github.com/numbertheory/chatgpt-go/chat"
 	"github.com/TwiN/go-color"
@@ -32,20 +33,33 @@ func main() {
 	token := os.Getenv("CHATGPT_TOKEN")
 	conversation := ""
 	response := ""
+	turn := 0
+	var record_interactions []string
 	if token == "" {
 		fmt.Printf(tokenNotSet)
 		os.Exit(1)
 	}
 	for {
-		userInput := chat.StringPrompt(color.InGreen("Human>"))
+		userInput := chat.StringPrompt(color.InGreen("(" + strconv.Itoa(turn) +")Human>"))
 		if userInput == "exit" {
 			break
 		}
-
+		record_interactions = append(record_interactions, userInput)
 		conversation = conversation + jsonEscape(userInput)
 		response = chat.SendChat(conversation, token)
 		fmt.Printf(color.InPurple("ChatGPT> ") + strings.TrimSpace(response) + "\n")
 		conversation = conversation + jsonEscape(response)
+		record_interactions = append(record_interactions, response)
+		turn += 1
+		// every 20th turn we will keep the first ten messages and the last ten messages to prevent the conversation from 
+		// overwhelming the API
+		if turn == 10 {
+			conversation = ""
+			conversation = conversation + jsonEscape(strings.Join(record_interactions[0:2], ""))
+			conversation = conversation + jsonEscape(strings.Join(record_interactions[8:10], ""))
+			record_interactions = []string{}
+			turn = 0
+		}
 	}
 
 }
